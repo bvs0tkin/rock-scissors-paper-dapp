986bb0ab858e09a5050ac2ce8694dc7c5c0055c8
Creampanda <andrewfromtokyo@gmail.com>
2023-03-23 11:02:19 +0000

    Added tests

--- test/Payments.test.js
+++ test/Payments.test.js
@@ -1,5 +1,7 @@
 const { expect } = require("chai");
+const { Wallet } = require("ethers");
 const { ethers } = require("hardhat");
+const { extendConfig } = require("hardhat/config");
 
 describe("Payments", function () {
   let acc1;
@@ -10,10 +12,39 @@ describe("Payments", function () {
     const Payments = await ethers.getContractFactory("Payments", acc1);
     payments = await Payments.deploy();
     await payments.deployed();
-    console.log(payments.address);
   });
 
   it("Should be deployed", async function () {
-    console.log("success!");
+    expect(payments.address).to.be.properAddress;
+  });
+
+  it("Should have 0 eth balance", async function () {
+    const balance = await payments.currentBalance();
+    expect(balance).to.eq(0);
+  });
+
+  it("Should send funds", async function () {
+    const tx = await payments.pay("hello from hardhat", { value: 100 });
+    await tx.wait();
+    const balance = await payments.currentBalance();
+    expect(balance).to.eq(100);
+  });
+
+  it("Should send funds from another acc", async function () {
+    const sum = 100;
+    const msg = "hello from hardhat";
+    const tx = await payments
+      .connect(acc2)
+      .pay("hello from hardhat", { value: sum });
+    await expect(() => tx).to.changeEtherBalances(
+      [acc2, payments],
+      [-sum, sum]
+    );
+    await tx.wait();
+
+    const newPayment = await payments.getPayment(acc2.address, 0);
+    expect(newPayment.message).to.eq(msg);
+    expect(newPayment.amount).to.eq(sum);
+    expect(newPayment.from).to.eq(acc2.address);
   });
 });
