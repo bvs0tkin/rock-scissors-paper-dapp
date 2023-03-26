2e4bd70ed3212bf335328893d3e7dd7459ed2d57
utebayev-mm <utebayev.mm@gmail.com>
2023-03-26 22:09:32 +0600

    further styling

--- frontend/src/components/screens/Home.tsx
+++ frontend/src/components/screens/Home.tsx
@@ -52,28 +52,34 @@ const Home = () => {
 
    return (
       <div className={styles.home}>
-         <div>Instructions:</div>
-         <div>1. Connect a Wallet</div> 
-         <div>2. Input TBNB quantity</div> 
-         <div>3. Choose a shape by clicking the corresponding button and confirm a transaction in your wallet</div>
+         <h1>ROCK PAPER SCISSORS</h1>
+         <div>By ♂DUNGEON MASTERS♂</div>
+         <h2>Instructions:</h2>
+         <p>1. Connect a Wallet</p> 
+         <p>2. Input TBNB quantity</p> 
+         <p>3. Choose a shape by clicking the corresponding button and confirm a transaction in your wallet</p>
          
-         <div>Address: {selectedAddress}</div>
-         <button class="button button5" onClick={() => connectWallet()}>Connect Wallet</button>
+         <div class="wallet">Connected wallet: {selectedAddress}</div>
+         <button class="button button1" onClick={() => connectWallet()}>Connect Wallet</button>
          <section>
-            <label for="fname">Input TBNB quantity (i.e. 0.001): </label>
+         <p></p>
+         <div class="box">
+            <p><label for="fname">Input TBNB quantity (i.e. 0.001): </label>
             <input
                id="fname" 
                name="fname" 
                type="text"
                onChange={(el) => setPlayValue(Number(el.target.value))}
-            />
-            <div>Choose your shape: </div>
-            <div>
-               <button class="button button1" onClick={() => playGame(0, playValue)}>Rock</button>
+            /></p>
+         </div>
+         <p></p>
+            <p class="box">Choose your shape: </p>
+            <div class="box">
+               <button class="button button5" onClick={() => playGame(0, playValue)}>Rock</button>
                <button class="button button2" onClick={() => playGame(1, playValue)}>Scissors</button>
                <button class="button button3" onClick={() => playGame(2, playValue)}>Paper</button>
             </div>
-            <div><span>Game result: {gameResult ? "Win" : "Lose"}</span></div>
+            <div><h2>Game result: {gameResult ? "You won!" : "You lost!"}</h2></div>
 
          </section>
       </div>
